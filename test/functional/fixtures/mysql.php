4151ee58452019dae4d6f622b7f471124f53cbda
andypotanin <andy.potanin@usabilitydynamics.com>
2014-09-10 19:32:53 -0400

    [ci skip]

---
+++ test/functional/fixtures/mysql.php
@@ -0,0 +1,9 @@
+<?php
+
+$ses = mysqli_connect("localhost","hipstack", "hipstack", "hipstack");
+
+if(!$ses){
+  die( json_encode( array( "ok" => false, "message" => "Connection did not work." ) ) );
+}
+
+die( json_encode( array( "ok" => true, "message" => "Connection to MySQL worked." ) ) );
