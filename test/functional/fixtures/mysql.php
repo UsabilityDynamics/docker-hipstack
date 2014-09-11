8f26dce01d4077ec4aa85ec5ec81c9514604e2d2
andypotanin <andy.potanin@usabilitydynamics.com>
2014-09-10 22:10:05 -0400

    [ci skip]

--- test/functional/fixtures/mysql.php
+++ test/functional/fixtures/mysql.php
@@ -1,13 +1,30 @@
 <?php
 header( 'content-type: application/json' );
 
+$_GET["host"] = $_GET["host"] ? $_GET["host"] : 'localhost';
+$_GET["user"] = $_GET["user"] ? $_GET["user"] : 'root';
+$_GET["password"] = $_GET["password"] ? $_GET["password"] : 'root';
+$_GET["database"] = $_GET["database"] ? $_GET["database"] : 'hipstack';
+
 $ses = mysqli_connect( $_GET["host"],$_GET["user"], $_GET["password"], $_GET["database"] );
 
 
 if(!$ses){
-  $_result = array( "ok" => false, "message" => "Connection did not work." );
+
+  $_result = array(
+    "ok" => false,
+    "message" => "Connection did not work.",
+    "creds" => $_GET
+  );
+
 } else {
-  $_result = array( "ok" => true, "message" => "Connection to MySQL worked." );
+
+  $_result = array(
+    "ok" => true,
+    "message" => "Connection to MySQL worked.",
+    "creds" => $_GET
+  );
+
 }
 
 die( json_encode( $_result, JSON_PRETTY_PRINT ) );
