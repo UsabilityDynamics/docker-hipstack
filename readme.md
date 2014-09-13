54951b59126af46bae1857770c13115f0e75a5c4
andypotanin <andy.potanin@usabilitydynamics.com>
2014-09-13 07:54:13 -0400

    tweaks to env-var settings. put hhvm into hipstack user

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 * If HHVM isn't starting try removing /var/run/hhvm.sock and /var/run/hhvm.pid.
 
+### Environment Variables
+```
+X-HIPSTACK-CONSTANTS={"DB_HOST":"localhost","DB_USER":"user","DB_PASSWORD":"password"}
+```
+
 ### Environment Settings
 ```
 export BUILD_ORGANIZATION=usabilitydynamics
--- static/etc/apache2/envvars.sh
+++ static/etc/apache2/envvars.sh
@@ -4,17 +4,38 @@
 ##
 ##
 ##
-
 # for supporting multiple apache2 instances
 if [ "${APACHE_CONFDIR##/etc/apache2-}" != "${APACHE_CONFDIR}" ] ; then
-	SUFFIX="-${APACHE_CONFDIR##/etc/apache2-}"
+  SUFFIX="-${APACHE_CONFDIR##/etc/apache2-}"
 else
-	SUFFIX=
+  SUFFIX=
 fi
 
 # Load all the system environment variables.
-. /etc/environment
+if [ -f "/etc/environment" ]; then
+  . /etc/environment
+fi;
 
+## Common
+export PHP_ENV
+export NODE_ENV
+export WP_ENV
+export APP_ENV
+
+export DB_NAME
+export DB_HOST
+export DB_USER
+export DB_PREFIX
+export DB_PASSWORD
+
+export DOCKER_HOST
+export DOCKER_PORT
+
+export WP_DEBUG
+export WP_SITEURL
+export WP_HOME
+
+## Default
 export APACHE_RUN_USER=hipstack
 export APACHE_RUN_GROUP=hipstack
 export APACHE_PID_FILE=/var/run/apache2/apache2$SUFFIX.pid
@@ -23,8 +44,3 @@ export APACHE_LOCK_DIR=/var/lock/apache2$SUFFIX
 export APACHE_LOG_DIR=/var/log/apache2$SUFFIX
 export LANG=C
 export LANG
-
-# export APACHE_LYNX='www-browser -dump'
-# export APACHE_ULIMIT_MAX_FILES='ulimit -n 65536'
-# export APACHE_ARGUMENTS=''
-# export APACHE2_MAINTSCRIPT_DEBUG=1
